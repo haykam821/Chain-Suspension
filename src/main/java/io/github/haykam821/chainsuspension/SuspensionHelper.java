7ffd7c8dd866b477f0d1bddcbfed36442324c94d
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-08-31 22:52:49 -0400

    Add a tag for blocks that cannot be suspended

--- src/main/java/io/github/haykam821/chainsuspension/SuspensionHelper.java
+++ src/main/java/io/github/haykam821/chainsuspension/SuspensionHelper.java
@@ -1,13 +1,25 @@
 package io.github.haykam821.chainsuspension;
 
+import net.fabricmc.fabric.api.tag.TagRegistry;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ChainBlock;
+import net.minecraft.tag.Tag;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
 public class SuspensionHelper {
+	private static final String MOD_ID = "chainsuspension";
+
+	private static final Identifier UNSUSPENDABLE_BLOCKS_ID = new Identifier(MOD_ID, "unsuspendable_blocks");
+	private static final Tag<Block> UNSUSPENDABLE_BLOCKS = TagRegistry.block(UNSUSPENDABLE_BLOCKS_ID);
+
 	public static boolean isSuspended(World world, BlockPos pos) {
+		BlockState state = world.getBlockState(pos);
+		if (state.isIn(UNSUSPENDABLE_BLOCKS)) return false;
+	
 		BlockState upState = world.getBlockState(pos.up());
 		return upState.getBlock() instanceof ChainBlock && upState.get(ChainBlock.AXIS) == Direction.Axis.Y;
 	}
