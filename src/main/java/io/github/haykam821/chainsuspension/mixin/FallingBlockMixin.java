d8966aa19e914491b42d5d67e76d92fc85ee2f78
Jakob Dickert <jakob.dickert@outlook.com>
2022-11-17 13:06:06 +0100

    changes for 1.19.2

--- src/main/java/io/github/haykam821/chainsuspension/mixin/FallingBlockMixin.java
+++ src/main/java/io/github/haykam821/chainsuspension/mixin/FallingBlockMixin.java
@@ -1,7 +1,5 @@
 package io.github.haykam821.chainsuspension.mixin;
 
-import java.util.Random;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,6 +10,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.FallingBlock;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
 
 @Mixin(FallingBlock.class)
 public class FallingBlockMixin {
